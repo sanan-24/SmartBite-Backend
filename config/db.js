b6429f61b652b1add243292c21a7c1b43c46c44c
Muhammad Sanan Nawaz <sanan@tdtrg.com>
2026-05-01 03:55:36 +0500

    changing for deployment

--- config/db.js
+++ config/db.js
@@ -1,14 +1,30 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGODB_URI);
+
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error(`Error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
+const mongoose = require("mongoose");
+
+let isConnected = false;
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
+  if (isConnected) return;
+
+  const db = await mongoose.connect(process.env.MONGODB_URI);
+
+  isConnected = db.connections[0].readyState;
+  console.log("MongoDB Connected ✅");
 };
 
 module.exports = connectDB;
